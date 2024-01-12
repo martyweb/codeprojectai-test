b1c581f61741db7f9d41dfdc1f5a5b46cc25c9c4
Marty <marty@martyweb.com>
2024-01-12 17:23:16 -0600

    initital load

---
+++ setup-venv.bat
@@ -0,0 +1,7 @@
+python -m venv .venv
+.venv\Scripts\activate.bat
+:: python3 -m venv venv
+:: source venv/bin/activate (for bash)
+:: venv/bin/activate
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
